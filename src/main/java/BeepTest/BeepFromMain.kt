9c86e88fbdf68504fa23d04aa607226d76ad8172
Laurits <lbrack17@student.aau.dk>
2019-09-24 10:23:54 +0200

    Refractor af theas mappestruktur

--- src/main/java/BeepTest/BeepFromMain.kt
+++ src/main/java/BeepTest/BeepFromMain.kt
@@ -3,7 +3,7 @@ package BeepTest
 import com.fazecast.jSerialComm.SerialPort
 
 fun main() {
-    val COM4 = SerialPort.getCommPorts()[1]
+    val COM4 = SerialPort.getCommPorts()[0]
     val messageLength = byteArrayOf(0x06, 0x00)
     val command = byteArrayOf(0x80.toByte(), 0x03, 0x0B, 0x02, 0xF4.toByte(), 0x01)
 
