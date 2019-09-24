4c5f22922faf8be947a31ed58f45618e3394825e
Laurits <lbrack17@student.aau.dk>
2019-09-24 13:46:40 +0200

    Importerede javaFX og TornadoFX

--- src/main/kotlin/BeepTest/BeepFromMain.kt
+++ src/main/kotlin/BeepTest/BeepFromMain.kt
@@ -1,8 +1,12 @@
 package BeepTest
 
 import com.fazecast.jSerialComm.SerialPort
+import javafx.*
+import tornadofx.*
 
 fun main() {
+    launch<MyApp>()
+
     val COM4 = SerialPort.getCommPorts()[0]
     val messageLength = byteArrayOf(0x06, 0x00)
     val command = byteArrayOf(0x80.toByte(), 0x03, 0x0B, 0x02, 0xF4.toByte(), 0x01)
@@ -11,4 +15,12 @@ fun main() {
     COM4.writeBytes(messageLength, messageLength.size.toLong())
     COM4.writeBytes(command, command.size.toLong())
     COM4.closePort()
+
+}
+class MyApp: App(MyView::class)
+class MyView: View() {
+    override val root = vbox {
+        button("Press me")
+        label("Waiting")
+    }
 }
